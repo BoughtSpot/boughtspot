51fa0df197958de131f42ab5ac224157c9f3632a
Claude Ayitey <clauditey@gmail.com>
2015-07-17 23:44:31 +0000

    Added the Procfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'pages#index'
+
+  get '/contact'  => 'pages#contact'
+  get '/services' => 'pages#services'
+  get '/about'    => 'pages#about'
+  get '/cases'    => 'pages#cases'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
